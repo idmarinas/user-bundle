d0a00fe88973c1e04ad74bfb3ea5645b83e6f767
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2024-12-01 22:28:53 +0100

    Override method getPath for use new structure of bundles

--- src/IdmUserBundle.php
+++ src/IdmUserBundle.php
@@ -1,19 +1,32 @@
 <?php
 
 /**
- * This file is part of Bundle "IdmUserBundle".
+ * Copyright 2023-2024 (C) IDMarinas - All Rights Reserved
  *
- * @see https://github.com/idmarinas/user-bundle/
+ * Last modified by "IDMarinas" on 1/12/24, 22:28
  *
- * @license https://github.com/idmarinas/user-bundle/blob/master/LICENSE.txt
+ * @project IDMarinas User Bundle
+ * @see     https://github.com/idmarinas/user-bundle
  *
- * @since 1.0.0
+ * @file    IdmUserBundle.php
+ * @date    20/12/2023
+ * @time    22:28
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
  */
 
 namespace Idm\Bundle\User;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use function dirname;
 
 class IdmUserBundle extends Bundle
 {
+	public function getPath (): string
+	{
+		return dirname(__DIR__);
+	}
 }
