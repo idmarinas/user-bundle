dba8f70f7f3c41540d8c605e877cb50173edf356
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2024-12-02 21:33:42 +0100

    Add EmailVerifier service

--- config/services.php
+++ config/services.php
@@ -1,19 +1,40 @@
 <?php
 
 /**
- * This file is part of Bundle "IdmUserBundle".
+ * Copyright 2023-2024 (C) IDMarinas - All Rights Reserved
  *
- * @see https://github.com/idmarinas/user-bundle/
+ * Last modified by "IDMarinas" on 2/12/24, 21:26
  *
- * @license https://github.com/idmarinas/user-bundle/blob/master/LICENSE.txt
+ * @project IDMarinas User Bundle
+ * @see     https://github.com/idmarinas/user-bundle
  *
- * @since 1.0.0
+ * @file    services.php
+ * @date    20/12/2023
+ * @time    21:26
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
  */
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-return static function (ContainerConfigurator $container)
-{
-    $container->services()
-    ;
+use Doctrine\ORM\EntityManagerInterface;
+use Idm\Bundle\User\Security\EmailVerifier;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Mailer\MailerInterface;
+use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
+
+return static function (ContainerConfigurator $container) {
+	$container
+		->services()
+		->set(EmailVerifier::class, EmailVerifier::class)
+		->args([
+			service(VerifyEmailHelperInterface::class),
+			service(MailerInterface::class),
+			service(EntityManagerInterface::class),
+			service(RequestStack::class),
+		])
+	;
 };
