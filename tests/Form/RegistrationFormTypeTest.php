134b5ec7b0f7088c47b2426657738ec27eaeff09
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2024-12-03 12:51:38 +0100

    Updated/Added by IDE

---
+++ tests/Form/RegistrationFormTypeTest.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Copyright 2024 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 2/12/24, 21:35
+ *
+ * @project IDMarinas User Bundle
+ * @see     https://github.com/idmarinas/user-bundle
+ *
+ * @file    RegistrationFormTypeTest.php
+ * @date    02/12/2024
+ * @time    21:35
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.1.0
+ */
+
+namespace Idm\Bundle\User\Tests\Form;
+
+use Idm\Bundle\Common\Traits\Tool\FakerTrait;
+use Idm\Bundle\User\Entity\AbstractUser;
+use Idm\Bundle\User\Form\AbstractRegistrationFormType;
+use Symfony\Component\Form\Test\Traits\ValidatorExtensionTrait;
+use Symfony\Component\Form\Test\TypeTestCase;
+
+class RegistrationFormTypeTest extends TypeTestCase
+{
+	use FakerTrait;
+	use ValidatorExtensionTrait;
+
+	public function testSubmitValidData ()
+	{
+		$password = $this->faker()->password();
+		$formData = [
+			'email'                          => $this->faker()->email,
+			'plainPassword[password]'        => $password,
+			'plainPassword[password_repeat]' => $password,
+			'termsAccepted'                  => true,
+			'privacyAccepted'                => true,
+		];
+
+		$entity = new User();
+		$form = $this->factory->create(RegistrationFormType::class, $entity);
+
+		$expected = (clone $entity)
+			->setEmail($formData['email'])
+			->setTermsAccepted($formData['termsAccepted'])
+			->setPrivacyAccepted($formData['privacyAccepted'])
+		;
+
+		$form->submit($formData);
+
+		$this->assertTrue($form->isSynchronized());
+		$this->assertEquals($expected, $form->getData());
+	}
+}
+
+class RegistrationFormType extends AbstractRegistrationFormType {}
+
+class User extends AbstractUser {}
