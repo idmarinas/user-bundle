3d429245761bb351e51dfe5e5b146f08d7611a6a
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2024-12-03 16:21:49 +0100

    First commit

---
+++ tests/Form/ResetPasswordFormTypeTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright 2024 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 3/12/24, 15:13
+ *
+ * @project IDMarinas User Bundle
+ * @see     https://github.com/idmarinas/user-bundle
+ *
+ * @file    ResetPasswordFormTypeTest.php
+ * @date    02/12/2024
+ * @time    21:35
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.1.0
+ */
+
+namespace Idm\Bundle\User\Tests\Form;
+
+use Idm\Bundle\Common\Traits\Tool\FakerTrait;
+use Idm\Bundle\User\Form\AbstractResetPasswordFormType;
+use Symfony\Component\Form\Test\Traits\ValidatorExtensionTrait;
+use Symfony\Component\Form\Test\TypeTestCase;
+
+class ResetPasswordFormTypeTest extends TypeTestCase
+{
+	use FakerTrait;
+	use ValidatorExtensionTrait;
+
+	public function testResetPasswordForm ()
+	{
+		$formData = [
+			'plainPassword' => [
+				'first'  => $this->faker()->password(),
+				'second' => $this->faker()->password(),
+			],
+		];
+
+		$form = $this->factory->create(ResetPasswordFormType::class, $formData);
+
+		$form->submit($formData);
+
+		$this->assertTrue($form->isSynchronized());
+		$this->assertTrue($form->isValid());
+		$this->assertEquals($formData['plainPassword']['first'], $form->get('plainPassword')->get('first')->getData());
+		$this->assertEquals($formData['plainPassword']['second'], $form->get('plainPassword')->get('second')->getData());
+	}
+}
+
+class ResetPasswordFormType extends AbstractResetPasswordFormType {}
