b0809b724e1cc3ef4c9bbae376305a4b8ddf90db
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2024-12-17 11:50:41 +0100

    First commit

---
+++ src/Repository/ResetPasswordRequestRepository.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Copyright 2024 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 08/12/2024, 20:22
+ *
+ * @project IDMarinas User Bundle
+ * @see     https://github.com/idmarinas/user-bundle
+ *
+ * @file    ResetPasswordRequestRepository.php
+ * @date    05/12/2024
+ * @time    21:38
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.1.0
+ */
+
+namespace Idm\Bundle\User\Repository;
+
+use DateTimeInterface;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Idm\Bundle\User\Entity\ResetPasswordRequest;
+use Idm\Bundle\User\Model\AbstractUser;
+use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordRequestInterface;
+use SymfonyCasts\Bundle\ResetPassword\Persistence\Repository\ResetPasswordRequestRepositoryTrait;
+use SymfonyCasts\Bundle\ResetPassword\Persistence\ResetPasswordRequestRepositoryInterface;
+
+/**
+ * @extends ServiceEntityRepository<ResetPasswordRequest>
+ */
+class ResetPasswordRequestRepository extends ServiceEntityRepository implements ResetPasswordRequestRepositoryInterface
+{
+	use ResetPasswordRequestRepositoryTrait;
+
+	public function __construct (ManagerRegistry $registry)
+	{
+		parent::__construct($registry, ResetPasswordRequest::class);
+	}
+
+	/**
+	 * @param AbstractUser $user
+	 */
+	public function createResetPasswordRequest (
+		object            $user,
+		DateTimeInterface $expiresAt,
+		string            $selector,
+		string            $hashedToken
+	): ResetPasswordRequestInterface {
+		return new ResetPasswordRequest($user, $expiresAt, $selector, $hashedToken);
+	}
+}
