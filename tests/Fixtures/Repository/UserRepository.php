129156f314b62dc387bc126973fa0b39cb4d31db
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2024-12-23 17:53:41 +0100

    First commit

---
+++ tests/Fixtures/Repository/UserRepository.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright 2024 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 23/12/2024, 17:50
+ *
+ * @project IDMarinas User Bundle
+ * @see     https://github.com/idmarinas/user-bundle
+ *
+ * @file    UserRepository.php
+ * @date    23/12/2024
+ * @time    17:50
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   2.0.0
+ */
+
+namespace Fixtures\Repository;
+
+use Idm\Bundle\User\Model\Repository\AbstractUserRepository;
+
+class UserRepository extends AbstractUserRepository {}
