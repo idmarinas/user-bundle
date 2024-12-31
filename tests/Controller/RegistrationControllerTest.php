76cf6be286e699cddc38a0f7861929fbf664694b
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2024-12-31 16:57:49 +0100

    test(controller): add test for sending password reset email exception
    Added a test case to verify that an exception is thrown when the
    mailer fails to send a password reset email in the
    ResetPasswordController.

--- tests/Controller/RegistrationControllerTest.php
+++ tests/Controller/RegistrationControllerTest.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2024 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 30/12/2024, 23:53
+ * Last modified by "IDMarinas" on 31/12/2024, 16:57
  *
  * @project IDMarinas User Bundle
  * @see     https://github.com/idmarinas/user-bundle
@@ -19,11 +19,26 @@
 
 namespace Idm\Bundle\User\Tests\Controller;
 
+use App\Repository\User\UserRepository;
 use DataFixtures\UserFixtures;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Factory\UserFactory;
+use Idm\Bundle\User\Controller\ResetPasswordController;
+use ReflectionException;
+use ReflectionMethod;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\Exception\TransportException;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordToken;
+use SymfonyCasts\Bundle\ResetPassword\ResetPasswordHelperInterface;
+use function Zenstruck\Foundry\faker;
 
 class RegistrationControllerTest extends WebTestCase
 {
@@ -185,5 +200,51 @@ class RegistrationControllerTest extends WebTestCase
 			$email,
 			'This link will expire in 1 hour, login is required to verify the email'
 		);
+	}
+
+	/**
+	 * @throws ReflectionException
+	 */
+	public function testProcessSendingPasswordResetEmailThrowsException ()
+	{
+		$client = static::createClient();
+		$client->request(Request::METHOD_GET, '/user/registration/register');
+		$mailer = $this->createMock(MailerInterface::class);
+		$translator = $this->createMock(TranslatorInterface::class);
+		$entityManager = $this->createMock(EntityManagerInterface::class);
+		$repository = $this->createMock(UserRepository::class);
+		$helper = $this->createMock(ResetPasswordHelperInterface::class);
+
+		$token = new ResetPasswordToken(
+			faker()->sha1(), new DateTime('+10 years'), (new DateTime('now'))->getTimestamp()
+		);
+		$helper->method('generateResetToken')->willReturn($token);
+		$repository->method('findOneBy')->willReturn(UserFactory::createOne()->_real());
+		$entityManager->method('getRepository')->willReturn($repository);
+
+		$mailer
+			->method('send')
+			->will($this->throwException(new TransportException()))
+		;
+
+		$requestStack = static::getContainer()->get('request_stack');
+		$requestStack->push($client->getRequest());
+
+		$client->getRequest()->getSession();
+		$container = new Container();
+		$container->set('mailer', $mailer);
+		$container->set('translator', $translator);
+		$container->set('request_stack', $requestStack);
+		$container->set('router', static::getContainer()->get('router'));
+
+		$controller = new ResetPasswordController($helper, $entityManager);
+		$controller->setContainer($container);
+
+		$method = new ReflectionMethod($controller, 'processSendingPasswordResetEmail');
+
+		$response = $method->invoke($controller, 'test@example.com', $mailer, $translator);
+
+		$this->assertInstanceOf(RedirectResponse::class, $response);
+		$this->assertEquals('/user/reset-password/reset', $response->getTargetUrl());
 	}
 }
