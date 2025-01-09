11ffd3e6b538c426c35179b840e1e5e3db269a51
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-09 21:51:22 +0100

    feat(bundles): add EasyAdminBundle to the configuration file

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 09/01/2025, 21:43
+ * Last modified by "IDMarinas" on 09/01/2025, 21:50
  *
  * @project IDMarinas User Bundle
  * @see     https://github.com/idmarinas/user-bundle
@@ -20,6 +20,7 @@
 use DAMA\DoctrineTestBundle\DAMADoctrineTestBundle;
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle;
+use EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle;
 use Idm\Bundle\User\IdmUserBundle;
 use Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
@@ -39,6 +40,7 @@ return [
 	SecurityBundle::class                  => ['all' => true],
 	StofDoctrineExtensionsBundle::class    => ['all' => true],
 	IdmUserBundle::class                   => ['all' => true],
+	EasyAdminBundle::class                 => ['all' => true],
 
 	// Dev-Test Bundles
 	MakerBundle::class                     => ['all' => true],
