084e4b5d27b513f96aefed4c2db89037af66bfaf
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-09 18:00:03 +0100

    refactor(user): replace UserPremium with Premium entity

---
+++ app/src/Entity/User/Premium.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 09/01/2025, 17:06
+ *
+ * @project IDMarinas User Bundle
+ * @see     https://github.com/idmarinas/user-bundle
+ *
+ * @file    Premium.php
+ * @date    09/01/2025
+ * @time    17:04
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   2.0.0
+ */
+
+namespace App\Entity\User;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Idm\Bundle\User\Model\Entity\AbstractPremium;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'idm_user_premium')]
+#[Gedmo\Loggable(logEntryClass: PremiumLog::class)]
+class Premium extends AbstractPremium {}
