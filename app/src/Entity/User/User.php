3fe69ea693f49f64743fb5642d6bf9bc34001406
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-09 17:57:11 +0100

    feat(user): update last modified date in User entity and add loggable annotation

--- app/src/Entity/User/User.php
+++ app/src/Entity/User/User.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Copyright 2024 (C) IDMarinas - All Rights Reserved
+ * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 30/12/2024, 17:17
+ * Last modified by "IDMarinas" on 09/01/2025, 17:56
  *
  * @project IDMarinas User Bundle
  * @see     https://github.com/idmarinas/user-bundle
@@ -21,14 +21,16 @@ namespace App\Entity\User;
 
 use App\Repository\User\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 use Idm\Bundle\User\Model\Entity\AbstractUser;
 use Idm\Bundle\User\Traits\Entity\IDMarinasProviderTrait;
 use Idm\Bundle\User\Traits\Entity\UserPremiumTrait;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: 'idm_user_user')]
+#[Gedmo\Loggable(logEntryClass: Log::class)]
 class User extends AbstractUser
 {
-	use IDMarinasProviderTrait;
-	use UserPremiumTrait;
+    use IDMarinasProviderTrait;
+    use UserPremiumTrait;
 }
