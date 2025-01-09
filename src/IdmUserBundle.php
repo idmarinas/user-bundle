92c90b494303635b3fe3df018a6694c2ad9a2cb7
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-09 19:17:32 +0100

    refactor: remove unused dependencies and configuration settings from IdmUserBundle

--- src/IdmUserBundle.php
+++ src/IdmUserBundle.php
@@ -3,7 +3,7 @@
 /**
  * Copyright 2023-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 09/01/2025, 18:21
+ * Last modified by "IDMarinas" on 09/01/2025, 19:03
  *
  * @project IDMarinas User Bundle
  * @see     https://github.com/idmarinas/user-bundle
@@ -20,27 +20,15 @@
 
 namespace Idm\Bundle\User;
 
-use Idm\Bundle\User\Repository\ResetPasswordRequestRepository;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 final class IdmUserBundle extends AbstractBundle
 {
 	public function prependExtension (ContainerConfigurator $container, ContainerBuilder $builder): void
 	{
 		$container->import(dirname(__DIR__) . '/config/rate_limiter.php');
-
-		$builder->prependExtensionConfig('symfonycasts_reset_password', [
-			'request_password_repository' => ResetPasswordRequestRepository::class,
-		]);
-
-		$builder->prependExtensionConfig('security', [
-			'password_hashers' => [
-				PasswordAuthenticatedUserInterface::class => 'auto',
-			],
-		]);
 	}
 
 	public function loadExtension (array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
