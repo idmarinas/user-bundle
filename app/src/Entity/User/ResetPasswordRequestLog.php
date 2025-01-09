683b8e61d18bd0c857202d474d87543bc9d8357e
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-09 17:57:58 +0100

    refactor(entity): rename ResetPasswordRequestLog and update namespace

--- app/src/Entity/User/ResetPasswordRequestLog.php
+++ app/src/Entity/User/ResetPasswordRequestLog.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 08/01/2025, 12:42
+ * Last modified by "IDMarinas" on 09/01/2025, 17:56
  *
  * @project IDMarinas User Bundle
  * @see     https://github.com/idmarinas/user-bundle
@@ -17,7 +17,7 @@
  * @since   2.0.0
  */
 
-namespace Idm\Bundle\User\Entity;
+namespace App\Entity\User;
 
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Loggable\Entity\MappedSuperclass\AbstractLogEntry;
@@ -25,10 +25,14 @@ use Gedmo\Loggable\Entity\Repository\LogEntryRepository;
 
 #[ORM\Entity(repositoryClass: LogEntryRepository::class)]
 #[ORM\Table(name: 'idm_user_reset_password_request_log', options: ['row_format' => 'DYNAMIC'])]
-#[ORM\Index(name: 'log_class_lookup_idx', columns: ['object_class'])]
-#[ORM\Index(name: 'log_date_lookup_idx', columns: ['logged_at'])]
-#[ORM\Index(name: 'log_user_lookup_idx', columns: ['username'])]
-#[ORM\Index(name: 'log_version_lookup_idx', columns: ['object_id', 'object_class', 'version'])]
+#[ORM\Index(name: 'idm_user_reset_password_request_log_class_lookup_idx', columns: ['object_class'])]
+#[ORM\Index(name: 'idm_user_reset_password_request_log_date_lookup_idx', columns: ['logged_at'])]
+#[ORM\Index(name: 'idm_user_reset_password_request_log_user_lookup_idx', columns: ['username'])]
+#[ORM\Index(name: 'idm_user_reset_password_request_log_version_lookup_idx', columns: [
+	'object_id',
+	'object_class',
+	'version',
+])]
 class ResetPasswordRequestLog extends AbstractLogEntry
 {
 	/* All required columns are mapped through inherited superclass */
