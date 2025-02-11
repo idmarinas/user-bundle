be6547ac09734ca8aa4aa534e637c105b48102a2
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-11 22:30:10 +0100

    feat(form): add RegistrationFormType class for user registration form

---
+++ app/src/Form/RegistrationFormType.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 11/02/2025, 22:28
+ *
+ * @project IDMarinas User Bundle
+ * @see     https://github.com/idmarinas/user-bundle
+ *
+ * @file    RegistrationFormType.php
+ * @date    11/02/2025
+ * @time    22:28
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   2.0.0
+ */
+
+namespace App\Form;
+
+use Idm\Bundle\User\Model\Form\AbstractRegistrationFormType;
+
+class RegistrationFormType extends AbstractRegistrationFormType {}
