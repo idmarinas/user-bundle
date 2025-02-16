3ebc9742768931cca4bd9a515354c49f70e2f2f8
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-16 19:11:19 +0100

    fix(trait): update EquatableTrait version annotation from 2.0.0 to 1.0.0

--- src/Traits/Entity/EquatableTrait.php
+++ src/Traits/Entity/EquatableTrait.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 11/01/2025, 10:58
+ * Last modified by "IDMarinas" on 16/02/2025, 19:11
  *
  * @project IDMarinas User Bundle
  * @see     https://github.com/idmarinas/user-bundle
@@ -14,7 +14,7 @@
  * @author  Iván Diaz Marinas (IDMarinas)
  * @license BSD 3-Clause License
  *
- * @since   2.0.0
+ * @since   1.0.0
  */
 
 namespace Idm\Bundle\User\Traits\Entity;
