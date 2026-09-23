51202ccfff36202d76e0f7495d322afced515db8
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-09-23 20:20:57 +0200

    refactor(tests): simplify bootstrap file by removing unused kernel and filesystem setup

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Copyright 2024 (C) IDMarinas - All Rights Reserved
+ * Copyright 2024-2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 29/12/2024, 23:50
+ * Last modified by "IDMarinas" on 23/09/2026, 20:14
  *
  * @project IDMarinas User Bundle
  * @see     https://github.com/idmarinas/user-bundle
@@ -17,18 +17,4 @@
  * @since   2.0.0
  */
 
-use App\Kernel;
-use Symfony\Component\Filesystem\Filesystem;
-
-require dirname(__DIR__) . '/vendor/autoload.php';
-
-$kernel = new Kernel('test', true);
-$filesystem = new Filesystem();
-
-if ($filesystem->exists($kernel->getCacheDir())) {
-	$filesystem->remove($kernel->getCacheDir());
-}
-
-if ($filesystem->exists($kernel->getLogDir())) {
-	$filesystem->remove($kernel->getLogDir());
-}
+require dirname(__DIR__).'/vendor/autoload.php';
